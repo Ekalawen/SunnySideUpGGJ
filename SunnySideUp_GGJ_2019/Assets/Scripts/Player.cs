c5afaa5e95b709759adcd47ffa731d3bfeabb944
Kevin Plumyoen <kevin.plumyoen@laposte.net>
2019-01-26 02:27:47 -0500

    Smoother jump

--- SunnySideUp_GGJ_2019/Assets/Scripts/Player.cs
+++ SunnySideUp_GGJ_2019/Assets/Scripts/Player.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 15.0f;
+    public float speed = 10.0f;
     public float jump_force = 12.0f;
     public float jump_time = 0.5f;
+    public float jump_coef = 2.8f;
 
     public float gravity = -9.81f;
 
@@ -30,11 +31,8 @@ public class Player : MonoBehaviour
 
         float translation = Input.GetAxis("Horizontal");
         
-        // Make it move 10 meters per second instead of 10 meters per frame...
         translation *= Time.deltaTime * speed;
-
-        // Move translation along the object's z-axis
-        //transform.Translate(0, 0, translation);
+        
         forces.x += translation;
         forces.y += gravity*Time.deltaTime;
 
@@ -43,9 +41,6 @@ public class Player : MonoBehaviour
         if (controller.isGrounded && Input.GetButtonDown("Jump"))
         {
             StartCoroutine(Jump());
-            Debug.Log("hey"     );
-
-            //m_Rigidbody.AddForce(new Vector3(0, jump_force, 0));
         }
 
         if (Input.GetButtonDown("Interact"))
@@ -58,10 +53,11 @@ public class Player : MonoBehaviour
 
     IEnumerator Jump()
     {
-        float finish_time = Time.time + jump_time;
-        while (Time.time < finish_time)
+        float remaining_time = jump_time;
+        while (remaining_time > 0.0f)
         {
-            controller.Move(new Vector3(0,jump_force*Time.deltaTime,0));
+            controller.Move(new Vector3(0,jump_force*Time.deltaTime*jump_coef*remaining_time,0));
+            remaining_time -= Time.deltaTime;
             yield return null;
         }
     }
